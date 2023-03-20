56c0bdadddbf26d815aa4460a17860ac565113cc
Bruno Gigante <brunogigante2001@gmail.com>
2023-03-20 16:08:28 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/gymclubapi/GymClubApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gymclubapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymClubApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<GymClubApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gymclubapi/GymClubApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gymclubapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GymClubApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
